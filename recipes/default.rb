d116323b385a2b41642781d7aeb06c1b3e1dd998
Will Milton <wa.milton@gmail.com>
2013-12-15 20:50:29 -0600

    whoops etc

--- recipes/default.rb
+++ recipes/default.rb
@@ -11,7 +11,6 @@ node.override.build_essential.compiletime = true
 include_recipe 'build-essential'
 include_recipe 'apache2'
 include_recipe 'apache2::mod_php5'
-include_recipe 'test_drupal'
 include_recipe 'postfix'
 
 drupal_config = data_bag_item('drupal', 'default').to_hash
