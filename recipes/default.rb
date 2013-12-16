bda347354dcd35562ee9393346a13ffb371d1f4b
Will Milton <wa.milton@gmail.com>
2013-12-16 11:33:09 -0600

    create all the base dirs in drupal::default

--- recipes/default.rb
+++ recipes/default.rb
@@ -12,11 +12,6 @@ include_recipe 'drupal::mass_virtual'
 include_recipe 'apache2::mod_php5'
 include_recipe 'postfix'
 
-directory '/var/www/sites' do
-  owner node.easy_drupal.drupal_user
-  group 'www-data'
-end
-
 drupal_config = data_bag_item('drupal', 'default').to_hash
 drupal_settings "#{node.drupal.settings_dir}/default" do
   config drupal_config
