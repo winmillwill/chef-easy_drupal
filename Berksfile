2ed727baba178eec269ca51079d6937d773aac70
Will Milton <wa.milton@gmail.com>
2013-12-15 20:03:57 -0600

    base

---
+++ Berksfile
@@ -0,0 +1,5 @@
+site :opscode
+
+metadata
+cookbook 'drupal', github: 'winmillwill/drupal_cookbook', branch: 'release-1.2.0'
+
---
+++ recipes/default.rb
@@ -0,0 +1,22 @@
+#
+# Cookbook Name:: easy_drupal
+# Recipe:: default
+#
+# Copyright (C) 2013 YOUR_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+node.override.build_essential.compiletime = true
+include_recipe 'build-essential'
+include_recipe 'apache2'
+include_recipe 'apache2::mod_php5'
+include_recipe 'test_drupal'
+include_recipe 'postfix'
+
+drupal_config = data_bag_item('drupal', 'default').to_hash
+drupal_settings "#{node.drupal.settings_dir}/default" do
+  config drupal_config
+  owner 'vagrant'
+  group 'www-data'
+end
